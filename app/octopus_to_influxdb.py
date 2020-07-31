796bca33774d147dc40685df6a8fd45e6887bf0b
Andrew Gillard <andrew@lorddeath.net>
2020-07-31 18:01:49 +0100

    Add some notes, to-dos, tweaks to the main script.

--- app/octopus_to_influxdb.py
+++ app/octopus_to_influxdb.py
@@ -34,8 +34,12 @@ def retrieve_paginated_data(
 def store_series(connection, series, metrics, rate_data):
 
     agile_data = rate_data.get('agile_unit_rates', [])
-    agile_rates = {
+    agile_rates_to = {
         point['valid_to']: point['value_inc_vat']
+        for point in agile_data
+    }
+    agile_rates_from = {
+        point['valid_from']: point['value_inc_vat']
         for point in agile_data
     }
 
@@ -43,6 +47,7 @@ def store_series(connection, series, metrics, rate_data):
         if series == 'gas':
             return 'unit_rate'
         elif not rate_data['unit_rate_low_zone']:  # no low rate
+            # TODO This way of determining the tariff type could do with improvement
             return 'unit_rate_high'
 
         low_start_str = rate_data['unit_rate_low_start']
@@ -82,7 +87,7 @@ def store_series(connection, series, metrics, rate_data):
         }
         if agile_data:
             agile_standing_charge = rate_data['agile_standing_charge'] / 48
-            agile_unit_rate = agile_rates.get(
+            agile_unit_rate = agile_rates_to.get(
                 maya.parse(measurement['interval_end']).iso8601(),
                 rate_data[rate]  # cludge, use Go rate during DST changeover
             )
@@ -120,6 +125,49 @@ def store_series(connection, series, metrics, rate_data):
     default="octograph.ini",
     type=click.Path(exists=True, dir_okay=True, readable=True),
 )
+# @TODO This whole script could do with a lot more comments to explain what's going on...
+# @TODO "yesterday midnight" will only work if this is run prior to midnight (give or take some DST stuff); I had a
+#        cron job running this at 1am and that caused Problems!
+# @TODO Add an option for JUST fetching Agile data. Will need to tweak the script so that Agile data can be inserted to
+#        Influx without any corresponding consumption data.
+# @TODO Fetch Agile data for next day if run after whatever ~4-5pm UTC time at which Agile data is made available.
+# @TODO All data is being stored with timestamps of the *end* of the 30-min period, which might make sense for
+#        consumption data, but makes it extremely difficult to plot Agile rates in Grafana (without Flux)!
+# @TODO Fetch more than one day of data by default so that we automatically fill in any gaps in the dataset caused by
+#        minor, temporary issues (e.g. with Octopus' API; with the smart meters; with this script or the hardware &
+#        Internet connection on which it relies; etc.). InfluxDB should simply update any existing records rather
+#        than duplicating data.
+# @TODO Would be nice to avoid adding 'cost' and 'total_cost' fields (which are for Go) when using Agile.
+# @TODO I'm pretty sure the active_rate_field() and tags_for_measurement() functions are inserting meaningless
+#        `active_rate`="unit_rate_high" tags into our Agile data.
+# @TODO Not sure what the point of the `time_of_day` tag is, either, since it essentially duplicates the record's
+#        standard timestamp:
+#            measurement-point = {
+#                'tags': [ 'time_of_day': maya.parse(measurement['interval_end']).datetime().strftime('%H:%M') ],
+#                'time': measurement['interval_end']
+#            } # (Abridged)
+#            Unless a time_of_day tag makes some queries easier?
+# @TODO Can we get rid of that "cludge" with DST changeover? Storing dates in agile_rates dict as unix timestamps or
+#        similar might help here?
+# @TODO Gas data is returned by the API in either kWh or m³ depending on the type of smart meter:
+#        "SMETS1 Secure" = kWh; "SMETS2" = m³.
+#        Reference: https://developer.octopus.energy/docs/api/#consumption
+#        The tariff charges for gas per kWh, so if data is returned as m³ (can this even be detected‽) it needs to be
+#         converted to kWh using:
+#            kWh = m³ * [calorific value] * 1.02264 / 3.6
+#        1.02264 is the "volume correction" to account for variations in temperature and pressure.
+#         It's a fixed value and is set by [the Gas (Calculation of Thermal Energy) Regulations 1996](https://www.legislation.gov.uk/uksi/1996/439/made/data.pdf),
+#         so it applies equally to everyone in the UK, as far as I can tell.
+#        "x / 3.6" is the standard conversion from MJ (megajoules) to kWh.
+#        The calorific value (in MJ/m³) varies over time and between customers, though "gas transporters are required
+#         to maintain this figure within 38 MJ/m³ to 41 MJ/m³" [https://www.gov.uk/guidance/gas-meter-readings-and-bill-calculation#step-3].
+#        This is a fairly narrow range, so simply using the midpoint of 39.5 would result in calculations having an
+#         error within 4%, which is probably sufficient for our purposes.
+#        My own bills between 2015 and 2020 have only had calorific values between 39.0 and 39.6, so 39.5 would
+#         always be within 1.5%.
+#        Alternatively a new configuration option could be added for it.
+#        We probably at least need a new config option to specify kWh or m³ metering.
+#        Reference: https://www.gov.uk/guidance/gas-meter-readings-and-bill-calculation
 @click.option('--from-date', default='yesterday midnight', type=click.STRING)
 @click.option('--to-date', default='today midnight', type=click.STRING)
 def cmd(config_file, from_date, to_date):
